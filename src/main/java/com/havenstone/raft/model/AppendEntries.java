c93ab8dc95cc01628fed2d4639a3e504207b427a
txat seq <tcat.seq@gmail.com>
2026-01-20 07:36:03 -0600

    - formatting changes

--- src/main/java/com/havenstone/raft/model/AppendEntries.java
+++ src/main/java/com/havenstone/raft/model/AppendEntries.java
@@ -3,6 +3,18 @@ package com.havenstone.raft.model;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Represents an AppendEntries RPC. Invoked by leader to replicate log entries
+ * and to provide a heartbeat.
+ * 
+ * @param term         Leader’s term
+ * @param leaderId     So follower can redirect clients
+ * @param prevLogIndex Index of log entry immediately preceding new ones
+ * @param prevLogTerm  Term of prevLogIndex entry
+ * @param entries      Log entries to store (empty for heartbeat; may send 
+ *                      more than one for efficiency)
+ * @param leaderCommit Leader’s commitIndex
+ */
 public record AppendEntries(
     long term,
     String leaderId,
--- src/main/java/com/havenstone/raft/model/AppendEntriesResult.java
+++ src/main/java/com/havenstone/raft/model/AppendEntriesResult.java
@@ -1,5 +1,15 @@
 package com.havenstone.raft.model;
 
-public record AppendEntriesResult() {
+import java.io.Serializable;
 
-}
+/**
+ * Represents the result of an AppendEntries RPC.
+ * 
+ * @param term    Current term, for leader to update itself.
+ * @param success True if follower contained entry matching prevLogIndex and 
+ *                  prevLogTerm.
+ */
+public record AppendEntriesResult(
+    long term,
+    boolean success
+) implements Serializable{}
