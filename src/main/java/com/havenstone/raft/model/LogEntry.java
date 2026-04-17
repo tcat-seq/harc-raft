287b5387947219aacbad1a72c39737497cecf51f
txat seq <tcat.seq@gmail.com>
2026-04-17 14:00:09 -0500

    - updating java docs

--- src/main/java/com/havenstone/raft/model/LogEntry.java
+++ src/main/java/com/havenstone/raft/model/LogEntry.java
@@ -3,7 +3,7 @@ package com.havenstone.raft.model;
 import java.io.Serializable;
 
 /**
- * Represents a single log entry in the Raft log.
+ * Represents a single log entry (command) in the Raft log.
  *
  * @param term    The term when the entry was received by the leader.
  * @param command The command to be applied to the state machine (arbitrary 
